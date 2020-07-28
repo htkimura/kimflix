7375ae9fa7d1f77f5abbda77161cc7f0f0909877
Henry <htkimura@gmail.com>
2020-07-28 16:42:18 -0300

    Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,15 +3,16 @@ import Logo from '../../assets/logo-kimflix.png'
 import './Menu.css'
 // import ButtonLink from '../components/ButtonLink'
 import Button from '../Button'
+import { Link } from 'react-router-dom'
 
 const Menu = () => {
 	return (
 		<nav className="Menu">
-			<a href="/">
+			<Link to="/">
 				<img className="Logo" src={Logo} alt="Kimflix logo"></img>
-			</a>
+			</Link>
 
-			<Button as="a" className="ButtonLink" href="/">
+			<Button as={Link} className="ButtonLink" to="/cadastro/video">
 				Novo Vídeo
 			</Button>
 		</nav>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import dadosIniciais from './data/dados_iniciais.json'
-import Menu from './components/Menu'
-import BannerMain from './components/BannerMain'
-import Carousel from './components/Carousel'
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json'
+import BannerMain from '../../components/BannerMain'
+import Carousel from '../../components/Carousel'
+import Footer from '../../components/Footer'
 
-function App() {
+function Home() {
 	return (
 		<div>
 			<Menu />
@@ -22,8 +23,10 @@ function App() {
 			<Carousel category={dadosIniciais.categorias[2]} />
 
 			<Carousel category={dadosIniciais.categorias[3]} />
+
+			<Footer />
 		</div>
 	)
 }
 
-export default App
+export default Home
